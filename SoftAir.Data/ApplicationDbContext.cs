cb74f42a68039de998e96e044b0b2c4de2e0159d
Mykola Maksymiv <mmaksymiv@netfully.com>
2021-07-02 08:42:04 +0300

    add ApplicationDbContext

---
+++ SoftAir.Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SoftAir.Data
+{
+    class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+    }
+}
